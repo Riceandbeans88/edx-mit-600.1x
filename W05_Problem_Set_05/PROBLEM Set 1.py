a45f10a3a68b084676ff53e1ad98b10f01f4f0c4
Sarah Price <42584516+Riceandbeans88@users.noreply.github.com>
2018-10-24 13:44:11 -0400

    Update PROBLEM Set 1.py

--- W05_Problem_Set_05/PROBLEM Set 1.py
+++ W05_Problem_Set_05/PROBLEM Set 1.py
@@ -5,20 +5,20 @@ Make tests for these conditions
 T/F
 
 1.1
-The ONLY thing we are interested in when designing programs is that it returns the correct answer.
+The ONLY thing we are interested in when designing programs is that it returns the correct answer. (False)
 
 1.2
 Roughly speaking, under the RAM model of computation, adding two numbers takes the same amount of time as dividing them.
 
 1.3
-When determining asymptotic complexity, we discard all terms except for the one with the largest growth rate.
+When determining asymptotic complexity, we discard all terms except for the one with the largest growth rate. (True)
 
 1.4
-Bisection search is an example of linear time complexity
+Bisection search is an example of linear time complexity (False)
 
 1.5
 For large values of n, an algorithm that takes 20000n^2 steps
-has better time complexity (takes less time) than one that takes 0.001n^5 steps
+has better time complexity (takes less time) than one that takes 0.001n^5 steps (True)
 
 
 '''
